a4310b38de57867054f518fa7b90da24b53d60fc
meggs1 <megan.schertz@gmail.com>
2020-10-28 23:00:00 -0500

    commit tree 5b65571efc01f84b7dca0cf064d62483642c301f

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    if name_hash.find do |k, v|
+            while v == 1 || v == 10
+                return k
+            end
+        end 
+    else name_hash.empty?
+        nil
+    end
 end
